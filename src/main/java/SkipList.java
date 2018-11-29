3505b310c8f9550e8db2ad2c6366f462a2d3b3f9
GalaXYDY <dannyglxy@gmail.com>
2018-11-30 01:50:04 +0300

    more to do

--- src/main/java/SkipList.java
+++ src/main/java/SkipList.java
@@ -1,3 +1,117 @@
-public class SkipList {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+class SkipList {
+
+    class Node {
+        int value;
+        Node left;
+        Node down;
+
+        Node(int x, Node down, Node left) {
+            this.value = x;
+            this.down = down;
+            this.left = left;
+        }
+
+        Node(int x) {
+            this(x, null, null);
+        }
+    }
+
+    private List<Node> head;
+    private double p;
+    private int size;
+    private Random random;
+
+    private SkipList(double p, int maxHeight) {
+        this.p = p;
+        this.head = new ArrayList<Node>();
+        this.size = 0;
+        if (maxHeight < 0) throw new IllegalArgumentException();
+        for (int i = 0; i < maxHeight; i++) {
+            head.add(new Node(0));
+            if (i > 0) {
+                head.get(i).down = head.get(i - 1);
+            }
+        }
+        random = new Random();
+    }
+
+    SkipList() {
+        this(0.5, 10);
+    }
+
+    private int randomHeight() {
+        int height = 0;
+        while (random.nextDouble() < p && height < head.size() - 1) height++;
+        return height + 1;
+    }
+
+    void add(int value) {
+        final int maxHeight = head.size();
+        Node node = head.get(maxHeight - 1);
+        int h = maxHeight - 1;
+        Node[] update = new Node[maxHeight];
+
+        while (node != null) {
+            if (node.left == null || value < node.left.value) {
+                update[h--] = node;
+                node = node.down;
+            } else {
+                node = node.left;
+            }
+        }
+        int currentHeight = randomHeight();
+        Node[] current = new Node[currentHeight];
+        for (h = 0; h < currentHeight; h++) {
+            current[h] = new Node(value);
+            if (h > 0) {
+                current[h].down = current[h - 1];
+            }
+        }
+        if (maxHeight < currentHeight) {
+            for (h = maxHeight; h < currentHeight; h++) {
+                head.add(new Node(0, head.get(h - 1), current[h]));
+            }
+            for (h = 0; h < maxHeight; h++) {
+                current[h].left = update[h].left;
+                update[h].left = current[h];
+            }
+        } else {
+            for (h = 0; h < currentHeight; h++) {
+                current[h].left = update[h].left;
+                update[h].left = current[h];
+            }
+            size++;
+        }
+    }
+
+    int size() {
+        return size;
+    }
+
+    int[] toArray() {
+        int[] values = new int[size];
+        int i = 0;
+        Node node;
+        for (node = head.get(0).left; node != null; node = node.left) {
+            values[i++] = node.value;
+        }
+        return values;
+    }
+
+    void print() {
+        for (int i = head.size() - 1; i >= 0; i--) {
+            Node node = head.get(i).left;
+            System.out.print("Level " + i + ": ");
+            while (node != null) {
+                System.out.print(node.value + " ");
+                node = node.left;
+
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ src/test/java/SkipListTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SkipListTest {
+    @Test
+    public void test() {
+        SkipList skipList = new SkipList();
+        int[] values = new int[]{23, 54, 34, 12, 3, 8, 39, 25, 13, 24, 7, 89, 90};
+        int[] result = new int[]{3, 7, 8, 12, 13, 23, 24, 25, 34, 39, 54, 89, 90};
+
+        for (int i : values) {
+            skipList.add(i);
+        }
+        skipList.print();
+
+        Assert.assertEquals(skipList.size(), result.length);
+        Assert.assertArrayEquals(result, skipList.toArray());
+    }
+}
